5f62ac54544f1f90d65f7f7d04c5aa6c63b9f3bc
Bastian <bastian.reiter@posteo.de>
2024-01-18 21:51:08 +0100

    Added some meta data objects

---
+++ Development/DataWork.R
@@ -0,0 +1,64 @@
+
+#==============================================================================#
+#------------------------------------------------------------------------------#
+#   TinkerLab: Data processing                                                 #
+#------------------------------------------------------------------------------#
+#==============================================================================#
+
+
+library(readxl)
+library(usethis)
+
+
+#===============================================================================
+# Meta Data: Cancer Coding
+#===============================================================================
+
+FilePath <- "Development/Data/TinkerLab_CancerCoding.xlsx"
+
+# Data on Cancer Grouping
+#~~~~~~~~~~~~~~~~~~~~~~~~
+CancerGrouping <- read_excel(path = FilePath,
+                             sheet = "CancerGrouping")
+
+# Save data in .rda-file and make it part of package
+use_data(CancerGrouping, overwrite = TRUE)
+
+
+# Data on Cancer Surgery
+#~~~~~~~~~~~~~~~~~~~~~~~
+CancerSurgery <- read_excel(path = FilePath,
+                            sheet = "CancerSurgery")
+
+# Save data in .rda-file and make it part of package
+use_data(CancerSurgery, overwrite = TRUE)
+
+
+
+#===============================================================================
+# Meta Data: OPS coding
+#===============================================================================
+
+FilePath <- "Development/Data/TinkerLab_OPSCoding.xlsx"
+
+# Data on OPS Coding
+#~~~~~~~~~~~~~~~~~~~
+OPSCodes <- read_excel(path = FilePath,
+                       sheet = "OPSCodes")
+
+# Save data in .rda-file and make it part of package
+use_data(OPSCodes, overwrite = TRUE)
+
+
+
+#===============================================================================
+# Meta Data: Working with P21 data
+#===============================================================================
+
+FilePath <- "Development/Data/TinkerLab_P21.xlsx"
+
+df_Meta_DischargeReasons <- read_excel(path = FilePath,
+                                       sheet = "DischargeReasons")
+
+df_Meta_Departments <- read_excel(path = here("Data/MetaData/MetaData_Project.xlsx"),
+                                  sheet = "Departments")
--- R/data.R
+++ R/data.R
@@ -20,6 +20,59 @@
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# CancerGrouping.rda
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#' Meta data on grouping of ICD-10 cancer codes
+#'
+#' A tibble
+#'
+#' @format ## 'CancerGrouping'
+#' Tibble
+#' \describe{
+#'   \item{ICD10CodeShort}{Three digit ICD-10 code}
+#'   \item{CancerTopographyGroup_ICD10}{Topography group as put forth by ICD-10}
+#'   \item{CancerTopographyDetail_ICD10}{Affected organ / topography as put forth by ICD-10}
+#'   \item{CancerTopographyGroup_ZFKD}{Topography detail as put forth by ZFKD}
+#'   \item{CancerTopographySpecification}{Additional information on topography}
+#'   \item{CancerSpecification}{Specification of cancer entity where needed}
+#'   \item{CancerIsLikelyToMetastasize}{}
+#'   \item{CancerIsCarcinomaInSitu}{}
+#'   \item{CancerIsNeoplasmOfUncertainBehavior}{}
+#' }
+#' @source <https://github.com/BastianReiter/TinkerLab/tree/main/Development/Data>
+"CancerGrouping"
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# CancerSurgery.rda
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#' Meta data on OPS codes in cancer surgery
+#'
+#' A tibble
+#'
+#' @format ## 'CancerSurgery'
+#' Tibble
+#' \describe{
+#'   \item{ICD10CodeShort}{Three digit ICD-10 code}
+#'   \item{CancerTopographyDetail_ICD10}{Affected organ / topography as put forth by ICD-10}
+#'   \item{OPSCodeShort}{Four digit OPS code}
+#'   \item{Procedure}{German label of procedure coded in OPS}
+#'   \item{IsLikelyCancerRelated}{}
+#'   \item{IsLikelyCurativeIntention_Primary}{}
+#'   \item{IsLikelyCurativeIntention_Secondary}{}
+#'   \item{IsLikelySupportive_Direct}{}
+#'   \item{IsLikelySupportive_Indirect}{}
+#'   \item{IsInPlattform65cCatalogue}{}
+#' }
+#' @source <https://github.com/BastianReiter/TinkerLab/tree/main/Development/Data>
+"CancerSurgery"
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Colors.rda
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -38,3 +91,18 @@
 
 
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# OPSCodes.rda
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#' Meta data on OPS codes in general
+#'
+#' A tibble
+#'
+#' @format ## 'OPSCodes'
+#' Tibble
+#' \describe{
+#'   \item{OPSVersion}{Year of OPS version}
+#' }
+#' @source <https://github.com/BastianReiter/TinkerLab/tree/main/Development/Data>
+"OPSCodes"
